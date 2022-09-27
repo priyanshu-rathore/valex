4842a481167b9cdd2eda1dd28b08ccf5ead918d6
priyanshu-rathore <priyansurathore132@gmail.com>
2022-09-27 19:05:53 +0530

    third commit

--- src/pages/Index/BarChart.jsx
+++ src/pages/Index/BarChart.jsx
@@ -50,7 +50,7 @@ const BarChart = () => {
       }
     
 
-    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     
     const data = {
         labels,
